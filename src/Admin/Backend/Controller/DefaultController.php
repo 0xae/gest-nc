89d0b5fb601b18d4a36e6aad77c870c12f78fedc
Ayrton Gomes <com.ayrton@gmail.com>
2018-02-03 12:52:38 -0100

    working on dashboard/stats

--- src/Admin/Backend/Controller/DefaultController.php
+++ src/Admin/Backend/Controller/DefaultController.php
@@ -6,6 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
 	public function indexAction() {
-		return $this->render('BackendBundle:Home:home.html.twig', array());
+		return $this->render('BackendBundle:Home:dashboard.html.twig', array());
 	}
 }
