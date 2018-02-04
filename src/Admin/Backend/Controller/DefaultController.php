f3da5b3ff9e2352dafab515a5a9a1adec364c121
Ayrton Gomes <com.ayrton@gmail.com>
2018-02-04 14:22:55 -0100

    working on dashboard/stats

--- src/Admin/Backend/Controller/DefaultController.php
+++ src/Admin/Backend/Controller/DefaultController.php
@@ -10,15 +10,120 @@ class DefaultController extends Controller {
 		return $this->render('BackendBundle:Home:dashboard.html.twig', array());
 	}
 
-	public function statsAction() {
-        $em = $this->getDoctrine()->getManager();
-		// $entities = $em->getRepository('BackendBundle:Complaint');
-		$qb = $em->createQueryBuilder();
-		$sql = "
-			select type,count(1) from complaint
-		";
+	public function statsAction($type) {
+		$year = 2018;
+		if ($type == 'by_department')
+			$db = $this->groupByDepartment($year);
+		else if ($type == 'by_month')
+			$db = $this->groupByMonth($year);
 
-		$ary = [];
-		return new JsonResponse($ary);
+		return new JsonResponse($db);
+	}
+
+	private function groupByDepartment($year) {
+		$complaints = '
+			select COUNT(c.type) as count,
+				c.type,
+				a.codigo as code
+			from complaint c
+			join app_entity a ON a.id = (select entity from user where id=c.created_by)
+			where year(c.created_at) = :year
+			group by c.type,a.codigo
+		';
+
+		$sugestions = '
+			select COUNT(c.type) as count,
+				c.type,
+				a.codigo as code
+			from sugestion c
+			join app_entity a ON a.id = (select entity from user where id=c.created_by)
+			where year(c.created_at) = :year
+			group by c.type,a.codigo
+		';
+
+		$ary = $this->fetchAll($complaints, ['year' => $year]);
+		$ary = array_merge($ary, $this->fetchAll($sugestions, ['year' => $year]));
+
+		$table = [];
+		foreach($ary as $val) {
+			$entry = [
+				'count' => $val['count'],
+				'type' => $val['type']	
+			];
+			$key = $val['code'];
+			if (array_key_exists($key, $table)) {
+				$table[$key][] = $entry;
+			} else {
+				$table[$key] = [$entry];
+			}
+		}
+
+		// $db = [];
+		// $deps = $this->fetchAll('select c.codigo as code,c.name from app_entity c', []);
+		// $obj = [
+		// 	['type'=>'queixa', 'count'=>0],
+		// 	['type'=>'denuncia', 'count'=>0],
+		// 	['type'=>'sugestao', 'count'=>0],
+		// 	['type'=>'reclamacao', 'count'=>0]
+		// ];
+
+		return ["departments" => $table];
+	}
+
+	private function groupByDay($year, $month) {		
+	}
+
+	private function groupByMonth($year) {
+        $em = $this->getDoctrine()->getManager();
+		$year = 2018;		
+		$sql = '
+			select COUNT(type) as count,
+				   DATE_FORMAT(created_at, "%Y-%m") as period,
+				   type 
+			from complaint 
+			where year(created_at) = :year
+			group by DATE_FORMAT(created_at, "%Y-%m"),type ;
+		';
+
+		$ary = $this->fetchAll($sql, ['year' => $year]);
+		$table = [];
+		foreach($ary as $val) {
+			$entry = [
+				'count' => $val['count'],
+				'type' => $val['type']	
+			];
+			$key = $val['period'];
+			if (array_key_exists($key, $table)) {
+				$table[$key][] = $entry;
+			} else {
+				$table[$key] = [$entry];
+			}
+		}
+
+		$obj = [
+			['type'=>'queixa', 'count'=>0],
+			['type'=>'denuncia', 'count'=>0],
+			['type'=>'sugestao', 'count'=>0],
+			['type'=>'reclamacao', 'count'=>0]
+		];
+		for ($i=1; $i<13; $i++) {
+			$key = "$year-" . str_pad($i, 2, '0', STR_PAD_LEFT);
+			if (!array_key_exists($key, $table)) {
+				$table[$key] = &$obj;
+			}
+		}
+
+		$db = [
+			"complaints" => $table			
+		];	
+
+		return $db;
+	}
+
+	private function fetchAll($sql, $params) {
+        $em = $this->getDoctrine()->getManager();
+		$stmt = $em->getConnection()->prepare($sql);
+		$stmt->execute($params);
+		return $stmt->fetchAll();		
 	}
 }
---
+++ src/Admin/Backend/Resources/public/js/app/controllers/dashboard.js
@@ -0,0 +1,246 @@
+angular.module("app")
+.controller("DashboardController", ['$http', '$scope', function ($http, $scope) {
+    console.info("--- init dash controller ---");
+    
+    function renderDepartments() {
+        fetchData('by_department')
+        .then(function (data) {
+            var deps = data.departments;
+            var keys = Object.keys(deps);
+            var series = {};
+            var types=["denuncia", "queixa", "reclamacao", "sugestao"];
+            var categories =[
+                "Denúncias",
+                "Queixas",
+                "Reclamações",
+                "Sugestões",
+            ];
+
+            keys.forEach(function (d){
+                series[d] = {
+                    name: d,
+                    data: []
+                };
+            });
+
+            keys.forEach(function (k){
+                var depTypes =_.sortBy(deps[k], function (d) { return d.type; });
+                types.forEach(function (t){
+                    var found=_.find(depTypes, function (dt) { return dt.type==t});
+                    if (found) {
+                        series[k].data.push(parseInt(found.count));
+                    } else {
+                        series[k].data.push(0);
+                    }
+                });
+            });
+
+            console.info("series", series);
+
+            var render = Object.keys(series).map(function (k){ return series[k]; });
+            renderBar(
+                'Queixas/Denúncias/Sugestões/Reclamações por Direções', 
+                'Ocorrencias',
+                'subtitulo',
+                'graph2',
+                categories,
+                render
+            );
+        });
+    }
+
+    function renderPerMonth() {
+        fetchData('by_month')
+        .then(function (data) {
+            var objects = data.complaints;
+            var keys = _.sortBy(Object.keys(objects))
+            var series = {
+                queixa: {
+                    name: "Queixa",
+                    data: []
+                },
+                denuncia:{
+                    name: "Denúncias",
+                    data: []
+                },
+                sugestao:{
+                    name: "Sugestao",
+                    data: []
+                },
+                reclamacao:{
+                    name: "Reclamacao",
+                    data: []
+                },
+            }
+    
+            keys.forEach(function (key){
+                var entry = objects[key];
+                entry.forEach(function (obj){
+                    if (!series[obj.type]) {          
+                        series[obj.type] = {name: obj.type, data: []};
+                    }
+                    series[obj.type].data.push(parseInt(obj.count));
+                });
+            });
+    
+            var render = Object.keys(series).map(function (k){ return series[k]; });
+    
+            var columns = [
+                'Janeiro',
+                'Fevereiro',
+                'Marco',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro'
+            ];
+    
+            renderBar('Queixas/Denúncias/Sugestões por Mês',
+                'Ocorrencias',
+                'subtitulo',
+                'graph1',
+                columns,
+                render
+            );    
+        });
+    }
+
+    renderPerMonth();
+    renderDepartments();
+
+    function fetchData(type) {
+        return $http.get('/arfa/web/app_dev.php/dashboard/stats/'+type)
+        .then(function (resp) {
+            return resp.data;
+        });
+    }
+
+    function renderGraph(xtitle, ytitle, subtitle, container, series) {
+        var seriesExample = [{
+            name: 'Installation',
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+        }, {
+            name: 'Manufacturing',
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+        }, {
+            name: 'Sales & Distribution',
+            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+        }, {
+            name: 'Project Development',
+            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+        }, {
+            name: 'Other',
+            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+        }];
+
+        Highcharts.chart(container, {
+            title: {
+                text: xtitle
+            },
+        
+            subtitle: {
+                text: subtitle
+            },
+        
+            yAxis: {
+                title: {
+                    text: ytitle
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+        
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 10
+                }
+            },
+
+            series: series,
+        
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+        
+        });    
+    }
+
+    function renderBar(xtitle, ytitle, subtitle, container, keys, series) {
+        var seriesExample = [{
+            name: 'Tokyo',
+            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    
+        }, {
+            name: 'New York',
+            data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+    
+        }, {
+            name: 'London',
+            data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+    
+        }, {
+            name: 'Berlin',
+            data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+    
+        }];
+
+        Highcharts.chart(container, {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: xtitle
+            },
+            subtitle: {
+                text: subtitle
+            },
+            xAxis: {
+                categories: keys,
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: ytitle
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.0f} '+ytitle+'</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.3,
+                    borderWidth: 0
+                }
+            },
+            series: series
+        });        
+    }
+}]);
