b1ffe0892041236ca120aaa459be71785190fab5
Ayrton Gomes <com.ayrton@gmail.com>
2018-03-08 10:35:52 -0100

    working on stuff

--- src/Admin/Backend/Resources/public/js/app/ireclamation/controller.js
+++ src/Admin/Backend/Resources/public/js/app/ireclamation/controller.js
@@ -1,19 +1,33 @@
 angular.module("app")
 .controller("IReclController", ['$http', '$scope','Admin', function ($http, $scope, Admin) {
-    $scope.viewIRecl = function (id, type) {
-        var labelX = 'Reclamacao Interna';
-
-        $scope.entity = undefined;
-        $http.get('/arfa/web/app_dev.php/administration/IReclamation/' + id +'/json')
+    function getRecl(id) {
+        return $http.get('/arfa/web/app_dev.php/administration/IReclamation/' + id +'/json')
         .then(function (resp){
-            var data = resp.data;
-            $scope.entity = data;
-            $scope.modalTitle = "Visualizando " + labelX;
-            $('#viewIRECLModal').modal();
+            return resp.data;
         }, function (error) {
             $.notify("A operacao nao pode ser efectuada.Tente novamente!", "danger");            
         });
     }
 
-}]);
+    $scope.viewIRecl = function (id, type) {
+        var labelX = 'Reclamacao Interna';
+        $scope.entity = undefined;
+        getRecl(id).then(function (data){
+            $scope.entity = data;
+            $scope.modalTitle = "Visualizando " + labelX;
+            $('#viewIRECLModal').modal();
+        });
+    }
 
+    $scope.favorable = function (id, label) {
+        getRecl(id).then(function (data){
+            console.info(data);
+        });
+    }
+
+    $scope.notFavorable = function (id, label) {
+        if (!confirm("Confirmar " + label + " como nao favoravel ?")){
+            return;
+        }
+    }
+}]);
