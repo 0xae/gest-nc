e1366484f55887cd576d95cbd09d46da3dae05f5
Ayrton Gomes <com.ayrton@gmail.com>
2018-04-05 11:45:25 -0100

    translate datables

--- src/Admin/Backend/Resources/public/js/app/app.js
+++ src/Admin/Backend/Resources/public/js/app/app.js
@@ -45,7 +45,23 @@ $(window).load(function() {
 });
 
 $(document).ready(function() {
-    $('.datatable').dataTable();
-    $("#DataTables_Table_0_previous").text("Anterior");
-    $("#DataTables_Table_0_next").text("Proximo");
+    $('.datatable').dataTable({
+        language: {
+            "sProcessing":   "A processar...",
+            "sLengthMenu":   "Mostrar _MENU_ registos",
+            "sZeroRecords":  "Não foram encontrados resultados",
+            "sInfo":         "Mostrando de _START_ até _END_ de _TOTAL_ registos",
+            "sInfoEmpty":    "Mostrando de 0 até 0 de 0 registos",
+            "sInfoFiltered": "(filtrado de _MAX_ registos no total)",
+            "sInfoPostFix":  "",
+            "sSearch":       "Procurar:",
+            "sUrl":          "",
+            "oPaginate": {
+                "sFirst":    "Primeiro",
+                "sPrevious": "Anterior",
+                "sNext":     "Seguinte",
+                "sLast":     "Último"
+            }
+        }        
+    });
 });
